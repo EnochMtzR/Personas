9e035836fd94ffca57ffa9e3030bf5a60585b269
Jose Enoc Martinez Rojas <josnocpp@gmail.com>
2018-08-09 11:24:35 -0500

    added methods setPeso and getPeso

--- personas.js
+++ personas.js
@@ -9,8 +9,17 @@ class Personas {
     saludar () {
         console.log(`Hola soy ${this.nombre} y tengo ${this.edad}`)
     }
+
+    setPeso (peso) {
+        this.peso = peso;
+    }
+
+    getPeso () { return this.peso }
 }
 
 let josias = new Personas("josias", 20, 1.75, 70);
 
 josias.saludar();
+
+josias.setPeso(60);
+console.log(`josias pesa ${josias.getPeso()} kg`);
