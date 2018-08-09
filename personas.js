5c74d1527e1e4263dea8493e606549ef08ed2ca4
JosJMR <josiasj.mr72@gmail.com>
2018-08-09 11:46:52 -0500

    added attribute especialidad and method desciribir

--- personas.js
+++ personas.js
@@ -1,9 +1,10 @@
 class Personas {
-    constructor(nombre, edad, estatura, peso) {
+    constructor(nombre, edad, estatura, peso, especialidad) {
         this.nombre = nombre;
         this.edad = edad;
         this.estatura = estatura;
         this.peso = peso;
+        this.especialidad = especialidad;
     }
 
     saludar () {
@@ -14,12 +15,17 @@ class Personas {
         this.peso = peso;
     }
 
+    describir () {
+        console.log(`Hola soy ${this.nombre} y estoy en la especialidad de ${this.especialidad}`);
+    }
+
     getPeso () { return this.peso }
 }
 
-let josias = new Personas("josias", 20, 1.75, 70);
+let josias = new Personas("josias", 20, 1.75, 70, "software");
 
 josias.saludar();
 
 josias.setPeso(60);
 console.log(`josias pesa ${josias.getPeso()} kg`);
+josias.describir();
