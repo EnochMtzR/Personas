023329f75c71e5631d1360ddb789c7bbd458292d
Jose Enoc Martinez Rojas <josnocpp@gmail.com>
2018-08-10 19:17:08 -0500

    added cabello features: cabello attribute and method luceCabello

--- personas.js
+++ personas.js
@@ -1,10 +1,11 @@
 class Personas {
-    constructor(nombre, edad, estatura, peso, especialidad) {
+    constructor(nombre, edad, estatura, peso, especialidad, cabello) {
         this.nombre = nombre;
         this.edad = edad;
         this.estatura = estatura;
         this.peso = peso;
         this.especialidad = especialidad;
+        this.cabello = cabello;
     }
 
     saludar () {
@@ -20,12 +21,16 @@ class Personas {
     }
 
     getPeso () { return this.peso }
+
+    luceCabello () {return this.cabello}
 }
 
-let josias = new Personas("josias", 20, 1.75, 70, "software");
+let josias = new Personas("josias", 20, 1.75, 70, "software", "negro");
 
 josias.saludar();
 
 josias.setPeso(60);
 console.log(`josias pesa ${josias.getPeso()} kg`);
 josias.describir();
+
+console.log(`tengo cabello ${josias.luceCabello()}`);
