29d192d22c5646745ac54a9bcaea4923963b827f
Jose Enoc Martinez Rojas <josnocpp@gmail.com>
2018-08-10 19:56:03 -0500

    created Variables.js

---
+++ variables.js
@@ -0,0 +1,23 @@
+let mensaje = "Hola mundo";
+let personas = 100;
+const pi = 3.1416;
+let booleanVar = true;
+let undefinedVar;
+
+// console.log(typeof(mensaje), typeof(numberVar), typeof(pi), typeof(booleanVar), typeof(undefinedVar));
+
+mensaje = 1;
+
+if (personas = 100){
+    let tontas = personas;
+    console.log(tontas);
+}
+
+for (let i=0; i<10; i++){
+    console.log('Hello World');
+}
+
+console.log(i, tontas);
+
+// console.log(typeof(mensaje), typeof(numberVar), typeof(pi), typeof(booleanVar), typeof(undefinedVar));
+// console.log(mensaje, numberVar, pi, booleanVar, undefinedVar);
