f7dea7056a282dc27916c4a62e3e08a92facaf26
Enoch Mtz <josnocpp@gmail.com>
2019-01-09 08:35:22 -0600

    Descomente esta linea porque se me antoja

--- variables.js
+++ variables.js
@@ -4,17 +4,23 @@ const pi = 3.1416;
 let booleanVar = true;
 let undefinedVar;
 
-// console.log(typeof(mensaje), typeof(numberVar), typeof(pi), typeof(booleanVar), typeof(undefinedVar));
+console.log(
+  typeof mensaje,
+  typeof numberVar,
+  typeof pi,
+  typeof booleanVar,
+  typeof undefinedVar
+);
 
 mensaje = 1;
 
-if (personas = 100){
-    let tontas = personas;
-    console.log(tontas);
+if ((personas = 100)) {
+  let tontas = personas;
+  console.log(tontas);
 }
 
-for (let i=0; i<10; i++){
-    console.log('Hello World');
+for (let i = 0; i < 10; i++) {
+  console.log("Hello World");
 }
 
 console.log(i, tontas);
